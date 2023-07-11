c8dd5016801128cc759080b9916bbc5ce4e9958b
ict318 <polychubu.ict@gmail.com>
2023-07-11 19:36:18 +0900

    P.196

--- app/src/main/java/com/websarva/wings/android/menusample/MainActivity.java
+++ app/src/main/java/com/websarva/wings/android/menusample/MainActivity.java
@@ -2,13 +2,122 @@ package com.websarva.wings.android.menusample;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ListView;
+import android.widget.SimpleAdapter;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ListView _lvMenu;
+    private List<Map<String, Object>> _menuList;
+    private static final String[] FROM ={"name", "price"};
+    private static final int[] TO ={R.id.tvMenuNameRow, R.id.tvMenuPriceRow};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        _lvMenu = findViewById(R.id.lvMenu);
+
+        _menuList = createTeishokuList();
+        //カレーメニューを追加
+        //_menuList.addAll(createCurryList());
+
+        SimpleAdapter adapter = new SimpleAdapter(MainActivity.this, _menuList, R.layout.row, FROM, TO);
+
+        _lvMenu.setAdapter(adapter);
+        _lvMenu.setOnItemClickListener(new ListItemClickListener());
+
     }
+
+
+    private List<Map<String, Object>> createTeishokuList(){
+        List<Map<String, Object>> menuList = new ArrayList<>();
+        Map<String, Object> menu = new HashMap<>();
+        menu.put("name", "から揚げ定食");
+        menu.put("price", 800);
+        menu.put("desc", "若鶏の唐揚げにサラダ、ご飯とみそ汁がつきます。");
+        menuList.add(menu);
+        menu = new HashMap<>();
+        menu.put("name", "ハンバーグ定食");
+        menu.put("price", 850);
+        menu.put("desc","手捏ねハンバーグにサラダ、ごはんとお味噌汁が付きます。");
+        menuList.add(menu);
+        menu = new HashMap<>();
+        menu.put("name", "生姜焼き定食");
+        menu.put("price", 850);
+        menu.put("desc","自家栽培のショウガを使用した生姜焼きにサラダ、ごはんとお味噌汁が付きます。");
+        menuList.add(menu);
+        menu = new HashMap<>();
+        menu.put("name", "ステーキ定食");
+        menu.put("price", 1000);
+        menu.put("desc","A5和牛のステーキにサラダ、ごはんとお味噌汁が付きます。");
+        menuList.add(menu);
+        menu = new HashMap<>();
+        menu.put("name", "野菜炒め定食");
+        menu.put("price", 750);
+        menu.put("desc","有機野菜を使用した野菜炒めにサラダ、ごはんとお味噌汁が付きます。");
+        menuList.add(menu);
+        menu = new HashMap<>();
+        menu.put("name", "とんかつ定食");
+        menu.put("price", 900);
+        menu.put("desc","三元豚のヒレとんかつにサラダ、ごはんとお味噌汁が付きます。");
+        menuList.add(menu);
+        menu = new HashMap<>();
+        menu.put("name", "ミンチカツ定食");
+        menu.put("price", 850);
+        menu.put("desc","うまみたっぷりのミンチカツにサラダ、ごはんとお味噌汁が付きます。");
+        menuList.add(menu);
+        menu = new HashMap<>();
+        menu.put("name", "チキンカツ定食");
+        menu.put("price", 900);
+        menu.put("desc","解放育成した若鳥のもも肉のカツにサラダ、ごはんとお味噌汁が付きます。");
+        menuList.add(menu);
+        menu = new HashMap<>();
+        menu.put("name", "コロッケ定食");
+        menu.put("price", 850);
+        menu.put("desc","ほくほくジャガイモのコロッケにサラダ、ごはんとお味噌汁が付きます。");
+        menuList.add(menu);
+        menu = new HashMap<>();
+        menu.put("name", "回鍋肉定食");
+        menu.put("price", 750);
+        menu.put("desc","回鍋肉にサラダ、ごはんとお味噌汁が付きます。");
+        menuList.add(menu);
+        menu = new HashMap<>();
+        menu.put("name", "麻婆豆腐定食");
+        menu.put("price", 800);
+        menu.put("desc","花椒が効いた麻婆豆腐にサラダ、ごはんとお味噌汁が付きます。");
+        menuList.add(menu);
+        return menuList;
+    }
+
+
+
+    private class ListItemClickListener implements AdapterView.OnItemClickListener {
+        @Override
+        public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+
+            Map<String, Object> item = (Map<String, Object>)parent.getItemAtPosition(position);
+
+            String menuName = (String)item.get("name");
+            Integer menuPrice = (Integer)item.get("price");
+
+            Intent intent = new Intent(MainActivity.this, MenuThanksActivity.class);
+            intent.putExtra("menuName", menuName);
+            intent.putExtra("menuPrice", menuPrice + "円");
+
+            startActivity(intent);
+        }
+    }
+
+
 }
--- app/src/main/java/com/websarva/wings/android/menusample/MenuThanksActivity.java
+++ app/src/main/java/com/websarva/wings/android/menusample/MenuThanksActivity.java
@@ -2,7 +2,9 @@ package com.websarva.wings.android.menusample;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MenuThanksActivity extends AppCompatActivity {
 
@@ -10,5 +12,21 @@ public class MenuThanksActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu_thanks);
+
+        //intentを受け取る
+        Intent intent = getIntent();
+        //送られてきた項目を取得
+        String menuName = intent.getStringExtra("menuName");
+        String menuPrice = intent.getStringExtra("menuPrice");
+
+        //画面部品の取得
+        TextView tvMenu = findViewById(R.id.tvMenuName);
+        TextView tvPrice = findViewById(R.id.tvMenuPrice);
+        //画面部品に値を設定
+        tvMenu.setText(menuName);
+        tvPrice.setText(menuPrice);
+
+        //ActionBar actionBar = getSupportActionBar();
+        //actionBar.setDisplayHomeAsUpEnabled(true);
     }
 }
