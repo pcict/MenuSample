a8e9f3f5a65e3111dc302c9cde5af9f364b192f2
ict318 <polychubu.ict@gmail.com>
2023-07-11 18:43:08 +0900

    P.193

---
+++ app/src/main/java/com/websarva/wings/android/menusample/MenuThanksActivity.java
@@ -0,0 +1,14 @@
+package com.websarva.wings.android.menusample;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MenuThanksActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_menu_thanks);
+    }
+}
